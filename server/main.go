6128401863a1f25ac3250f71b19df07c01928d65
Mohammad-Nawazish-Khan <Mohammad_Nawazish_Khan@external.mckinsey.com>
2019-10-19 17:24:09 +0530

    First Commit

---
+++ server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/nawazish-github/kramp/handlers"
+)
+
+//ToDo: Config file for no of response elements
+
+func main() {
+	r := gin.Default()
+	r.GET("/query", handlers.QueryHandler)
+	log.Fatal(r.Run())
+}
