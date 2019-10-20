eb0592831fba10e6656f2fb543419c2a6ff440f0
Mohammad-Nawazish-Khan <Mohammad_Nawazish_Khan@external.mckinsey.com>
2019-10-20 13:13:44 +0530

    Read configuration upfront

--- server/main.go
+++ server/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"log"
 
+	"github.com/nawazish-github/kramp/models"
+
 	"github.com/gin-gonic/gin"
 	"github.com/nawazish-github/kramp/handlers"
+	"github.com/nawazish-github/kramp/json"
 )
 
 //ToDo: Config file for no of response elements
+var Conf models.Config
 
 func main() {
+	Conf = json.UnmarshalConfig()
 	r := gin.Default()
 	r.GET("/query/:searchString", handlers.QueryHandler)
 	log.Fatal(r.Run())
